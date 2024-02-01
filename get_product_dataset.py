7d61b4d6fb9a7cef52769a59d9e484cea08e5a84
Davide Liu <davide97ls@gmail.com>
2024-02-01 16:52:39 +0800

    save catalog to csv

--- get_product_dataset.py
+++ get_product_dataset.py
@@ -1,17 +1,30 @@
+import os
 from scraper_costco import CostcoScraper
 from scraper_tmall import TmallScraper
 from scraper_jd import JDScraper
 from scraper_taobao import TaobaoScraper
 import pandas as pd
+import datetime
 
 
 if __name__ == "__main__":
-
     product_name = 'kinder bueno'
+    save_dir = 'results'
     vendors = [TaobaoScraper(), TmallScraper(), JDScraper()]
-    catalogs = [vendor.get_product_df(product_name) for vendor in vendors]  # TODO try ang except
+    catalogs = []
+    for vendor in vendors:
+        try:
+            df = vendor.get_product_df(product_name)
+            catalogs.append(df)
+        except:
+            print(f'Scraping failed for vendor: {vendor.store_name}')
 
     df = pd.concat(catalogs)
-    df = df[['vendor', 'name', 'price']]  # TODO fix dataset
-
-    print(df)
+    df = df[['vendor', 'name', 'price']]
+    df = df.reset_index(drop=True)
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M")
+    df_path = os.path.join(save_dir, f'catalog_{timestamp}.csv')
+    df.to_csv(df_path, index=False)
+    print(f'Catalog saved to {df_path}')
+    print(df.head(30))
